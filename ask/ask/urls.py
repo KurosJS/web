d30bd10768728afa02d01bb288e7103cadd72832
Kuros <93669487+KurosJS@users.noreply.github.com>
2022-10-22 15:02:38 +0300

    Add files via upload

---
+++ ask/ask/urls.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+]
+from django.contrib import admin
+from django.conf.urls import url, include
+
+from qa import views
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.test),
+    url(r'^login/$', views.test),
+    url(r'^signup/$', views.test),
+    url(r'^question/\d+/$', views.test),
+    url(r'^ask/$', views.test),
+    url(r'^popular/$', views.test),
+    url(r'^new/$', views.test),
+]
---
+++ etc/qa.py
@@ -0,0 +1,3 @@
+pythonpath = 'homeboxwebask'  # путь до дефолтного django-приложения
+bind = 0.0.0.08000
+workers = 4
